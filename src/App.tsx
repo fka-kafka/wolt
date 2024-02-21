84b79e5a325af9f04d6b1a30d4eda9823eb71462
the-code-papi <kigenbrandon@gmail.com>
2024-02-21 16:42:40 +0300

    created sample UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
+import { useState } from "react";
+
 function App() {
+
+  type CalculatorType = {
+    cart_value: number,
+    delivery_distance: number,
+    number_of_items: number,
+    time: string
+  }
+
+  type CalculatorStateType = { cart: CalculatorType}
+
+  const initState: CalculatorStateType = { cart: {} }
+
+  const [cart, setCart] = useState<CalculatorStateType>(initState)
+
+  const handleSubmit = () => {
+    
+  }
+
   return (
-    <>
-      <h1>Lock in.</h1>
-    </>
+    <section className="calculator">
+      <h1 className="header">Delivery Fee calculator</h1>
+
+      <form action="" className="to__deliver">
+        <div>
+          <label htmlFor="value">Cart value (€):</label>
+          <input
+            id="value"
+            type="text"
+            className="to__deliver-value"
+            placeholder="0.00"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="distance">Delivery distance (m):</label>
+          <input
+            id="distance"
+            type="text"
+            className="to__deliver-distance"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="items">Cart items: </label>
+          <input
+            id="items"
+            type="text"
+            className="to__deliver-items"
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="date">Delivery date: </label>
+          <input id="date" type="date" className="to__deliver-date" required />
+        </div>
+
+        <button type="submit" onSubmit={handleSubmit}>Calculate</button>
+      </form>
+    </section>
   );
 }
 
